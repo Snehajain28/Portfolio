fc413152e264bb098e72b5826b6a8fa087ce558c
Snehajain28 <snehajain28022004@gmail.com>
2024-04-27 21:28:00 +0530

    fixing

--- src/component/Contact.jsx
+++ src/component/Contact.jsx
@@ -42,7 +42,7 @@ const Contact = () => {
 		});
 	}
 
-	
+
 	return (
 		<div onClick={() => {
 			dispatch({
@@ -50,71 +50,70 @@ const Contact = () => {
 				hamburger: false,
 			})
 		}}
-			id='contact' className="w-full h-content mx-auto  ">
-			<div className="w-10/12 mx-auto flex flex-col justify-center items-center">
-				<h2 className='text-4xl font-bold w-[15rem] my-10 flex justify-center mx-auto border-b-4 border-gray-400'>
-					CONTACT ME
-				</h2>
-				<p className='lg:w-6/12 w-[90vw] text-justify '>Feel free to Contact me by submitting the form below or connect on social media and I will get back to you as soon as possible</p>
-				<ul className="space-y-5  my-10">
-					{contacts.map((contact) => (
-						<li className="flex gap-2 items-center" key={contact.id}>
-							<i className="text-2xl dark:text-gray-400 mr-4">
-								{contact.icon}
-							</i>
-							<span className="text-lg  dark:text-ternary-light">
-								{contact.name}
-							</span>
-						</li>
-					))}
-				</ul>
-				<div className="">
-					<div className="mx-auto ">
-						<div className="border-[2px] md:w-[30rem] rounded-lg p-4 mx-auto dark:bg-secondary-dark rounded-xl shadow-xl ">
-							<p className="font-general-medium border-b-[2px] w-[10rem] mx-auto text-center pb-2 dark:text-primary-light text-2xl mb-8">
-								Contact Form
-							</p>
-							<div className="mb-4 rounded-lg md:w-[15rem] md:my-10 md:mx-auto bg-white">
-								<TextField
-									label="Name"
-									variant="outlined"
-									size="small"
-									required
-									name="name"
-									value={formData.name}
-									onChange={changeHandler}
-									className='w-full'
-								/>
-							</div>
-							<div className="mb-4 rounded-lg md:w-[15rem] md:mx-auto  bg-white">
-								<TextField
-									label="Email"
-									variant="outlined"
-									size="small"
-									required
-									name="email"
-									value={formData.email}
-									onChange={changeHandler}
-									className='w-full'
-								/>
-							</div>
-							<div className="mb-4 rounded-lg md:w-[15rem] md:mx-auto md:my-10  bg-white  ">
-								<TextField
-									label="Message"
-									variant="outlined"
-									size="small"
-									required
-									name="msg"
-									value={formData.msg}
-									onChange={changeHandler}
-									className='w-full'
-								/>
-							</div>
+			id='contact' className="w-[90vw] mx-auto pb-10 h-content  mt-[4rem]  ">
+			<h2 className='text-4xl font-bold w-[15rem] my-10 flex justify-center mx-auto border-b-4 border-gray-400'>
+				CONTACT ME
+			</h2>
+			<p className='lg:w-6/12 lg:w-[60vw] mx-auto w-[90vw] text-justify '>Feel free to Contact me by submitting the form below or connect on social media and I will get back to you as soon as possible</p>
+		
+			<div className="w-10/12  mx-auto flex flex-col lg:flex-row lg:gap-10 justify-center items-center">
+				<div>
+					<ul className="space-y-5  my-10">
+						{contacts.map((contact) => (
+							<li className="flex gap-2 items-center" key={contact.id}>
+								<i className="text-2xl dark:text-gray-400 mr-4">
+									{contact.icon}
+								</i>
+								<span className="text-lg  dark:text-ternary-light">
+									{contact.name}
+								</span>
+							</li>
+						))}
+					</ul>
+				</div>
+				<div data-aos="fade-up" className="border-[2px] mx-auto  md:w-[30rem] rounded-lg p-4 mx-auto dark:bg-secondary-dark rounded-xl shadow-xl ">
+					<p className="font-general-medium border-b-[2px] w-[10rem] mx-auto text-center pb-2 dark:text-primary-light text-2xl mb-8">
+						Contact Form
+					</p>
+					<div className="mb-4 rounded-lg md:w-[15rem] md:my-10 md:mx-auto bg-white">
+						<TextField
+							label="Name"
+							variant="outlined"
+							size="small"
+							required
+							name="name"
+							value={formData.name}
+							onChange={changeHandler}
+							className='w-full'
+						/>
+					</div>
+					<div className="mb-4 rounded-lg md:w-[15rem] md:mx-auto  bg-white">
+						<TextField
+							label="Email"
+							variant="outlined"
+							size="small"
+							required
+							name="email"
+							value={formData.email}
+							onChange={changeHandler}
+							className='w-full'
+						/>
+					</div>
+					<div className="mb-4 rounded-lg md:w-[15rem] md:mx-auto md:my-10  bg-white  ">
+						<TextField
+							label="Message"
+							variant="outlined"
+							size="small"
+							required
+							name="msg"
+							value={formData.msg}
+							onChange={changeHandler}
+							className='w-full'
+						/>
+					</div>
 
-							<div className="font-general-medium mx-auto w-40 px-4 py-2.5  text-center font-medium tracking-wider bg-indigo-500 hover:bg-indigo-600 focus:ring-1 focus:ring-indigo-900 rounded-lg mt-6 duration-500">
-							<a href="mailto:snehajain28022004@gmail.com?body:hey"><button>Submit</button></a>	
-							</div>
-						</div>
+					<div className="font-general-medium mx-auto w-40 px-4 py-2.5  text-center font-medium tracking-wider bg-indigo-500 hover:bg-indigo-600 focus:ring-1 focus:ring-indigo-900 rounded-lg mt-6 duration-500">
+						<a href="mailto:snehajain28022004@gmail.com?body:hey"><button>Submit</button></a>
 					</div>
 				</div>
 			</div>
--- src/component/TechStack.jsx
+++ src/component/TechStack.jsx
@@ -14,13 +14,13 @@ export default function TechStack() {
               hamburger: false,
             })
           }}
-        id="tech" className=' my-[2rem] dark:bg-gradient-to-b from-gray-800 to-black dark:text-white w-full h-content'>
+        id="tech" className=' my-[2rem] dark:bg-gradient-to-b lg:w-[70vw] mx-auto from-gray-800 to-black dark:text-white w-full h-content'>
             <div className='max-w-screen-lg h-full p-4 mx-auto flex flex-col justify-center w-full '>
                 <div className='pb-4'>
                     <p className='text-4xl font-bold w-[14rem] flex justify-center mx-auto border-b-4 border-gray-400'>TECH STACK</p>
-                    <p className='py-6 '>These are the technologies I've worked with</p>
+                    <p className='py-10'>These are the technologies I've worked with</p>
                 </div>
-                <div className='w-[90vw] mx-auto h-full flex flex-wrap gap-5 lg:gap-10 lg:w-[80vw] justify-center text-center '>
+                <div className='w-[90vw] mx-auto h-full flex flex-wrap gap-5 lg:gap-20 lg:w-[70vw] justify-center text-center '>
                     {
                         Tech_data.map((tech, i) => (
                             <div data-aos="zoom-in"   key={i} className={`shadow-xl cursor-pointer h-[8rem]  w-[8rem] duration-500 py-2 rounded-lg dark:shadow-${tech.color}`}>
